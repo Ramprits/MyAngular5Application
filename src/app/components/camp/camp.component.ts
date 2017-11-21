45ab7f5b16c4b45c8a43e6f6f94bf0da19666f12
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-11-21 18:09:29 +0530

    camps api added

--- src/app/components/camp/camp.component.ts
+++ src/app/components/camp/camp.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CampService } from './camp.service';
+import { ICamp } from './icamp';
 
 @Component({
   selector: 'b-camp',
   templateUrl: './camp.component.html'
 })
 export class CampComponent implements OnInit {
-
-  constructor() { }
+  camps: ICamp[];
+  constructor(private campService: CampService) { }
 
   ngOnInit() {
+    this.GetCamps();
   }
-
+  GetCamps() {
+    this.campService.getCamps().subscribe(c => { this.camps = c; }, (err) => { console.log(err); });
+  }
 }
